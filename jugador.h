b70da46dce50d74e454bdaeed79114a0a64df081
peppa-peddler <jorenmija@gmail.com>
2021-12-11 17:25:25 -0500

    0wO

--- jugador.h
+++ jugador.h
@@ -3,32 +3,32 @@
 using namespace std;
 
 class Jugador{
- private:
+private:
 
-  int rows;
-  int cols;
-  Mapa *mapa;
-  char **enemigo;
+	int rows;
+	int cols;
+	Mapa *mapa;
+	char **enemigo;
 
- public:
+public:
 
-  Jugador(){
-	rows = cols = 10;
-	mapa = new Mapa(rows, cols);
-	mapa -> generar();
-	enemigo = new char*[ rows ];
-	for( int i = 0; i < rows ; i++ ){
-	  enemigo[ i ] = new char[ cols ];
-	  for( int j = 0 ; j < cols ; j++ )
-		enemigo[ i ][ j ] = ' ';
+	Jugador(){
+		rows = cols = 10;
+		mapa = new Mapa(rows, cols);
+		mapa -> generar();
+		enemigo = new char*[ rows ];
+		for( int i = 0; i < rows ; i++ ){
+			enemigo[ i ] = new char[ cols ];
+			for( int j = 0 ; j < cols ; j++ )
+				enemigo[ i ][ j ] = ' ';
+		}
 	}
-  }
 
-  string jugada();
+	string jugada();
 
-  void setRespuesta(point coords, char respuesta){
-	enemigo[ coords.F ][ coords.C ] = respuesta;
-  }
+	void setRespuesta(point coords, char respuesta){
+		enemigo[ coords.F ][ coords.C ] = respuesta;
+	}
 
 	void mostrarEnemigo(){
 
@@ -42,27 +42,27 @@ class Jugador{
 		for( int i = 0; i < rows ; i++ ){
 			cout << i + 1 << ( i + 1 < 10 ? espacio + " " : espacio );
 			for( int j = 0; j < cols ; j++ )
-			  cout << enemigo[ i ][ j ] << espacio;
+				cout << enemigo[ i ][ j ] << espacio;
 			cout << endl;
 		}
 
-  }
+	}
 
 	char responder( point coords ){
 		return mapa -> ataque( coords );
 	}
 
-  void mostrar(){
-	mapa -> mostrar();
-  }
+	void mostrar(){
+		mapa -> mostrar();
+	}
 
 
-  ~Jugador(){
-	delete mapa;
-	for( int i = 0; i < rows ; i++ )
-	  delete enemigo[ i ];
-	delete enemigo;
-	rows = cols = 0;
-  }
+	~Jugador(){
+		delete mapa;
+		for( int i = 0; i < rows ; i++ )
+			delete enemigo[ i ];
+		delete enemigo;
+		rows = cols = 0;
+	}
 
 };
