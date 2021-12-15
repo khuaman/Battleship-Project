71cfe7d49ca08aaaa4aaa06d8d6191d8aefeddc4
peppa-peddler <jorenmija@gmail.com>
2021-12-15 18:07:46 -0500

    updating

--- main.cpp
+++ main.cpp
@@ -5,27 +5,17 @@ int main() {
 
 	srand(time(NULL));
 
-	Jugador* Angel = new Jugador("FirstPlayer", "Team Angel");
+	Jugador* Angel = new Jugador("C:\\Users\\Usuario\\CODIGO\\SecondPlayer", "Team Angel");
 	Angel->empezar(); Angel->registrar();
 
-	Jugador* Kevin = new Jugador("SecondPlayer", "Team Kevin");
-	Kevin->empezar(); Kevin->registrar();
-
-	while (true) {
-		if (Angel->turno()) {
-			if (Angel->jugar() == false) break;
-		}
-		else if (Kevin->turno()) {
-			if (Kevin->jugar() == false) break;
-		}
-		else break;
-		Angel->mostrarEnemigo();
-		Kevin->mostrarEnemigo();
-		cout << "--" << endl;
-	}
+	 while (true) {
+        if (Angel->turno()) {
+            if (Angel->jugar() == false) break;
+        }
+        cout << "--" << endl;
+    }
 
 	delete Angel;
-	delete Kevin;
 
 	return 0;
 }
